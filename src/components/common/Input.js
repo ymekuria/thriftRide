032eca990b258718964c72334217e5f1caeb1e78
Yoni <ymekuria@gmail.com>
2016-10-27 16:04:46 -0700

    added spinner component

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,13 +1,12 @@
 
 import React from 'react';
-import { TextInput, View, Text } from 'react-native';
+import { TextInput, View } from 'react-native';
 
 const Input = ({ label, value, onChangeText, placeholder, secureTextEntry, autoCapitalize }) => {
   const { inputStyle, labelStyle, containerStyle } = styles;
 
   return (
     <View style={containerStyle}>
-      <Text style={labelStyle}>{label}</Text>
       <TextInput
         autoCapitalize="none"
         secureTextEntry={secureTextEntry}
