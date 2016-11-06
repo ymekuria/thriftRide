93f3e84e04221d651f3cd0eb6eb44ad4e608e54a
Yoni <ymekuria@gmail.com>
2016-11-06 13:31:00 -0800

    added ReduxThunk middlware

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMidleware } from 'redux';
+import ReduxThunk from 'redux-thunk'
 import reducers from './reducers';
 import { Header } from './components/common';
 import HomeView from './components/HomeView';
 
 class App extends Component {
   render() {
+    const store = createStore(reducers, {}, applyMidleware(ReduxThunk));
+    
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <View>
           <Header headerText="Hello World" />
           <HomeView />
