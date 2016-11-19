7bc5213013446960b070598358403cc4d91ab88c
Yoni <ymekuria@gmail.com>
2016-11-18 19:49:16 -0800

    created a router component and initialized firebase

---
+++ src/Router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Actions, Scene, Router } from 'react-native-router-flux';
+
+const RouterComponent = () => {
+  return (
+    <Router>
+
+    </Router>
+  );
+
+
+export default RouterComponent;
